b539d288c315392c7aaa39aeb955c8f09b02da63
unknown <everton.pratika@gmail.com>
2021-04-13 15:56:33 -0300

    segundo

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
-		double cat1,cat2,hip;
+		double cat1,catoposto,hip;
 		System.out.println("digite o valor do cateto");
 		cat1=teclado.nextDouble();
 		cat1=Math.pow(cat1, 2.0);
 		System.out.println("digite o valor do cateto oposto");
-		cat2=teclado.nextDouble();
-		cat2=Math.pow(cat2, 2.0);
-		hip=(Math.pow(cat1, 2)+Math.pow(cat2,2));
-	
+		catoposto=teclado.nextDouble();
+		hip=Math.sqrt(Math.pow(cat1,2)+ Math.pow(catoposto, 2));
 		
 		
-		System.out.println(" o valor da hipotenusa é "+ Math.sqrt(hip));
+		System.out.println(" o valor da hipotenusa é "+ hip);
 		
 		
 		
